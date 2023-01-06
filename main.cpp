8603d660be8f7c9ab767c68a61fafdee079ee628
khk811 <hyunkyungkim811@gmail.com>
2023-01-06 21:39:20 +0900

    vector 생성 소멸 테스트 코드 추가

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include "vector.hpp"
 
-int	main(void) {
+void	test(void) {
 	ft::vector<int>	test;
 
-	std::cout << test.size() << std::endl;
+	std::cout << "instance test size: " << test.size() << std::endl;
+	// test[0] = 42;
+	// std::cout << "test[0]: " << test[0] << std::endl;
+}
+
+int	main(void) {
+	test();
+	std::cout << "\n\n===Check Memory leaks===\n" << std::endl;
+	system("leaks ft_containers");
 	return 0;
 }
